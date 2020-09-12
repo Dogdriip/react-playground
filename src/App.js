d11772c04855c969bd571fbbad22e0b5cddd26d6
Hyunseung Jeon <dogdriip@gmail.com>
2020-09-12 23:33:08 +0900

    New hook: useTabs

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import React, { useState } from "react";
-import "./styles.css";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value }
-    } = event;
-    let willUpdate = true;
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-    if (willUpdate) {
-      setValue(value);
-    }
+const contents = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1!"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2!"
+  },
+  {
+    tab: "Section 3",
+    content: "I'm the content of the Section 3!"
+  }
+];
+
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
   };
-  return { value, onChange };
 };
 
 const App = () => {
-  const maxLenValidator = (value) => value.length <= 10;
-  const name = useInput("", maxLenValidator);
+  const { currentItem, changeItem } = useTabs(0, contents);
   return (
     <>
       <h1>Hello!</h1>
-      <input placeholder="Name" {...name} />
+      {contents.map((content, idx) => (
+        <button onClick={() => changeItem(idx)}>{content.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </>
   );
 };

fdeaf305cadfc4438eb6b141c8d1b7ea44f7d6f6
Hyunseung Jeon <dogdriip@gmail.com>
2020-09-13 00:00:27 +0900

    Introduction to useEffect

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
-import React, { useState } from "react";
-
-const contents = [
-  {
-    tab: "Section 1",
-    content: "I'm the content of the Section 1!"
-  },
-  {
-    tab: "Section 2",
-    content: "I'm the content of the Section 2!"
-  },
-  {
-    tab: "Section 3",
-    content: "I'm the content of the Section 3!"
-  }
-];
-
-const useTabs = (initialTab, allTabs) => {
-  if (!allTabs || !Array.isArray(allTabs)) {
-    return;
-  }
-
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
-  return {
-    currentItem: allTabs[currentIndex],
-    changeItem: setCurrentIndex
-  };
-};
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const { currentItem, changeItem } = useTabs(0, contents);
+  const sayHello = () => console.log("Hello!");
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  useEffect(sayHello, [x]);
   return (
     <>
       <h1>Hello!</h1>
-      {contents.map((content, idx) => (
-        <button onClick={() => changeItem(idx)}>{content.tab}</button>
-      ))}
-      <div>{currentItem.content}</div>
+      <button onClick={() => setX(x + 1)}>{x}</button>
+      <button onClick={() => setY(y + 1)}>{y}</button>
     </>
   );
 };

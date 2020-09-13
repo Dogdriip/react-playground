df3031f873506c9635f24eb62df5e979879b5171
Hyunseung Jeon <dogdriip@gmail.com>
2020-09-13 18:48:11 +0900

    New hook: useClick (using useRef, useEffect)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const App = () => {
-  const input = useRef();
-  useEffect(() => {
-    setTimeout(() => (input.current.value = "Hello"), 3000);
-  }, []); // only executes at componentDidMount
+const useClick = (onClick) => {
+  if (typeof onClick !== "function") return;
 
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, [onClick]);
+  return element;
+};
+
+const App = () => {
+  const sayHello = () => console.log("Hello!");
+  const title = useClick(sayHello);
   return (
     <>
-      <h1>Hello!</h1>
-      <input ref={input} placeholder="Name" />
+      <h1 ref={title}>Hello!</h1>
     </>
   );
 };
